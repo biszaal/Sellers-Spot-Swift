8124b3b0a790cefc8de03136ae24604117e952cb
Bishal Aryal <biszaal@yahoo.com>
2020-09-10 21:33:42 +0545

    Initial Commit

---
+++ Sellers Spot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sellers Spot
+//
+//  Created by Bishal Aryal on 20/9/10.
+//  Copyright © 2020 Bishal Aryal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
